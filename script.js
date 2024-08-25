9c8739f159b2c52c9cd604e7885d3f6262c9244a
kaliffathullah <145640401+kaliffathullah@users.noreply.github.com>
2024-08-25 22:05:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// script.js
+function answerCall() {
+    alert('Call Answered!');
+    // Here you could add more functionality like redirecting to another page
+    // or displaying an in-call interface.
+}
